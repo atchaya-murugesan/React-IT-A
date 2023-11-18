7aec983ca0fdd944c5e26da39d850c43a4742476
Atchaya <150216177+atchaya-murugesan@users.noreply.github.com>
2023-11-18 12:52:28 +0530

    DAY-5

---
+++ DAY 5/reactapp2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TestOne from './component/TestOne';
+import TestThree from './component/TestThree';
+import TestTwo from './component/TestTwo';
+function App() {
+  return (
+    <div className="App">
+      <TestOne/>
+      <TestTwo/>
+      <TestThree/>
+    </div>
+  );
+}
+
+export default App;
---
+++ DAY 5/reactapp2/src/component/TestTwo.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+export default class TestTwo extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Component Two</h1>
+            </div>
+        );
+    }
+}
